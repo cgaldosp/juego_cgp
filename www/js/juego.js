73f01aea7c1bb49809f0a6f44b3734ce93faab62
Carlos Galdos <cgaldosp@gmail.com>
2018-04-06 11:26:40 -0500

    verision miriadax

--- www/js/juego.js
+++ www/js/juego.js
@@ -5,10 +5,10 @@ var app={
     velocidadX = 0;
     velocidadY = 0;
     puntuacion = 0;
-    
+
     alto  = document.documentElement.clientHeight;
     ancho = document.documentElement.clientWidth;
-    
+
     app.vigilaSensores();
     app.iniciaJuego();
   },
@@ -19,29 +19,31 @@ var app={
       game.physics.startSystem(Phaser.Physics.ARCADE);
 
       game.stage.backgroundColor = '#f27d0c';
-      game.load.image('bola', 'assets/bola.png');
-      game.load.image('objetivo', 'assets/objetivo.png');
+      game.load.image('bola', 'assets/pac_man.png');
+      game.load.image('objetivo', 'assets/cerveza.jpg');
     }
 
     function create() {
       scoreText = game.add.text(16, 16, puntuacion, { fontSize: '100px', fill: '#757676' });
-      
-      objetivo = game.add.sprite(app.inicioX(), app.inicioY(), 'objetivo');
-      bola = game.add.sprite(app.inicioX(), app.inicioY(), 'bola');
-      
+
+      objetivo = game.add.sprite(app.inicioX(), 0, 'objetivo');
+      bola = game.add.sprite(ancho/2 - DIAMETRO_BOLA/ 2, alto, 'bola');
+
       game.physics.arcade.enable(bola);
       game.physics.arcade.enable(objetivo);
 
       bola.body.collideWorldBounds = true;
-      bola.body.onWorldBounds = new Phaser.Signal();
-      bola.body.onWorldBounds.add(app.decrementaPuntuacion, this);
+      objetivo.body.collideWorldBounds = true;
+      objetivo.body.onWorldBounds = new Phaser.Signal();
+      objetivo.body.onWorldBounds.add(app.decrementaPuntuacion, this);
     }
 
     function update(){
       var factorDificultad = (300 + (dificultad * 100));
       bola.body.velocity.y = (velocidadY * factorDificultad);
       bola.body.velocity.x = (velocidadX * (-1 * factorDificultad));
-      
+      objetivo.body.velocity.y = ((dificultad + 1) * 100);
+
       game.physics.arcade.overlap(bola, objetivo, app.incrementaPuntuacion, null, this);
     }
 
@@ -50,20 +52,36 @@ var app={
   },
 
   decrementaPuntuacion: function(){
-    puntuacion = puntuacion-1;
+    puntuacion = puntuacion-10;
+    dificultad = 0;
+    if("vibrate" in window.navigator) {
+      window.navigator.vibrate(100);
+    }
+    objetivo.body.x = app.inicioX();
+    objetivo.body.y = 0;
+    if (puntuacion < -20){
+      alert('Perdió la partida. Siga intentando');
+      puntuacion = 0;
+      app.recomienza;
+    }
     scoreText.text = puntuacion;
   },
 
   incrementaPuntuacion: function(){
     puntuacion = puntuacion+1;
-    scoreText.text = puntuacion;
 
     objetivo.body.x = app.inicioX();
-    objetivo.body.y = app.inicioY();
+    objetivo.body.y = 0;
 
     if (puntuacion > 0){
       dificultad = dificultad + 1;
     }
+    if (puntuacion == 5){
+      alert('Felicitaciones, ganó.');
+      puntuacion = 0;
+      app.recomienza;
+    }
+    scoreText.text = puntuacion;
   },
 
   inicioX: function(){
@@ -79,7 +97,7 @@ var app={
   },
 
   vigilaSensores: function(){
-    
+
     function onError() {
         console.log('onError!');
     }
@@ -95,10 +113,6 @@ var app={
   detectaAgitacion: function(datosAceleracion){
     var agitacionX = datosAceleracion.x > 10;
     var agitacionY = datosAceleracion.y > 10;
-
-    if (agitacionX || agitacionY){
-      setTimeout(app.recomienza, 1000);
-    }
   },
 
   recomienza: function(){
@@ -107,7 +121,7 @@ var app={
 
   registraDireccion: function(datosAceleracion){
     velocidadX = datosAceleracion.x ;
-    velocidadY = datosAceleracion.y ;
+    velocidadY = 0 ;
   }
 
 };
